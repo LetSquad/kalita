6d2e961b73499c49aacf22f412e4a261cbd3c247
Ivan Savoskin <ivan.savoskin@noones.team>
2023-02-10 20:06:10 +0300

    Исправление замечаний на ревью

--- src/utils/currencyUtils.ts
+++ src/utils/currencyUtils.ts
@@ -3,22 +3,30 @@ import { Currency } from "../models/portfolios/enums";
 
 export function moexCurrencyToInternalCurrency(moexCurrency: MoexCurrency): Currency {
     switch (moexCurrency) {
-        case MoexCurrency.RUB: { return Currency.RUB;
+        case MoexCurrency.RUB: {
+            return Currency.RUB;
         }
-        case MoexCurrency.USD: { return Currency.USD;
+        case MoexCurrency.USD: {
+            return Currency.USD;
         }
     }
 }
 
 export function getSymbol(currency: Currency | DividendCurrency) {
     switch (currency) {
-        case Currency.RUB: case DividendCurrency.RUB: { return "₽";
+        case Currency.RUB:
+        case DividendCurrency.RUB: {
+            return "₽";
         }
-        case Currency.CNY: { return "¥";
+        case Currency.CNY: {
+            return "¥";
         }
-        case Currency.USD: case DividendCurrency.USD: { return "$";
+        case Currency.USD:
+        case DividendCurrency.USD: {
+            return "$";
         }
-        case Currency.EUR: { return "€";
+        case Currency.EUR: {
+            return "€";
         }
     }
 }
